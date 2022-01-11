481dbc374ff930ee7020c1532b8f60faab2c7345
Rune Nielsen <runenielsen@runbox.com>
2022-01-11 20:27:33 +0100

    moved ChangeEvent to its own file

---
+++ src/KonstantDataValidator/ChangeEvent.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace KonstantDataValidator;
+
+public enum Operation
+{
+    Create,
+    Update,
+    Delete
+}
+
+public record ChangeEvent
+{
+    public TableWatch Tables { get; init; }
+    public IReadOnlyDictionary<string, object> Fields { get; init; }
+    public Operation Operation { get; init; }
+    public int ObjectId =>
+        (int)(Operation == Operation.Delete ? Fields["SDE_DELETES_ROW_ID"] : Fields["OBJECTID"]);
+    public long StateId => (long)Fields["SDE_STATE_ID"];
+
+    public ChangeEvent(TableWatch tables, IReadOnlyDictionary<string, object> columns, Operation operation)
+    {
+        Tables = tables;
+        Fields = columns;
+        Operation = operation;
+    }
+}
--- src/KonstantDataValidator/Listen.cs
+++ src/KonstantDataValidator/Listen.cs
@@ -51,31 +51,6 @@ public record TableWatch
     }
 }
 
-public enum Operation
-{
-    Create,
-    Update,
-    Delete
-}
-
-public record ChangeEvent
-{
-    public TableWatch Tables { get; init; }
-    public IReadOnlyDictionary<string, object> Fields { get; init; }
-    public Operation Operation { get; init; }
-    public int ObjectId => (int)(Operation == Operation.Delete
-                                 ? Fields["SDE_DELETES_ROW_ID"]
-                                 : Fields["OBJECTID"]);
-    public long StateId => (long)Fields["SDE_STATE_ID"];
-
-    public ChangeEvent(TableWatch tables, IReadOnlyDictionary<string, object> columns, Operation operation)
-    {
-        Tables = tables;
-        Fields = columns;
-        Operation = operation;
-    }
-}
-
 public class Listen : IListen
 {
     private readonly string _connectionString;
