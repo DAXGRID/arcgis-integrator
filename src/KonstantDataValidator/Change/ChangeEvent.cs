a40b37497ddfca30f3a451fa2fa792cd3ae55f2f
Rune Nielsen <runenielsen@runbox.com>
2022-01-13 13:18:23 +0100

    now checks different fields for deleted events (#22)

--- src/KonstantDataValidator/Change/ChangeEvent.cs
+++ src/KonstantDataValidator/Change/ChangeEvent.cs
@@ -15,9 +15,12 @@ public record ChangeEvent
     public TableWatch Tables { get; init; }
     public IReadOnlyDictionary<string, object> Fields { get; init; }
     public Operation Operation { get; init; }
-    public int ObjectId =>
-        (int)(Operation == Operation.Delete ? Fields["SDE_DELETES_ROW_ID"] : Fields["OBJECTID"]);
-    public long StateId => (long)Fields["SDE_STATE_ID"];
+    public int ObjectId => (int)(Operation == Operation.Delete
+                                 ? Fields["SDE_DELETES_ROW_ID"]
+                                 : Fields["OBJECTID"]);
+    public long StateId => (long)(Operation == Operation.Delete
+                                  ? Fields["DELETED_AT"]
+                                  : Fields["SDE_STATE_ID"]);
 
     public ChangeEvent(TableWatch tables, IReadOnlyDictionary<string, object> columns, Operation operation)
     {
